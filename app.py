9a980ada83fc16a3643d39a26bfaeae66e85f172
Soubhik Sarkar <134215202+CoderSoubhik@users.noreply.github.com>
2023-08-21 12:32:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,32 @@
-from flask import Flask, Response
-import pyttsx3
+from flask import Flask, request, jsonify
 import webbrowser
 
 app = Flask(__name__)
 
-def speak(text):
-    engine = pyttsx3.init("sapi5")
-    voices = engine.getProperty('voices')
-    engine.setProperty('voices', voices[0].id)
-    engine.setProperty('rate', 170)
-    print("")
-    print(f"AI: {text}.")
-    print("")
-    engine.say(text)
-    engine.runAndWait()
-
 def handle_user_input(user_input):
     if "hello" in user_input:
-        speak("Hello, how can I assist you?")
+        speak("Hello sir, how are you")
+        response = "Greeting spoken"
     elif "play" in user_input:
         video_url = "https://youtu.be/dQw4w9WgXcQ"
-        webbrowser.open(video_url)
-        speak("Playing YouTube video")
-    elif "exit" in user_input:
-        speak("Exiting")
+        play_video(video_url)
+        response = "Playing YouTube video"
     else:
-        speak("I don't understand that command.")
+        speak("I don't understand.")
+        response = "Command not recognized"
 
-@app.route('/<user_input>', methods=['GET'])
-def process_input(user_input):
-    handle_user_input(user_input)
-    return Response(status=204)
+    return response
+
+@app.route('/process_input', methods=['GET'])
+def process_input():
+    user_input = request.args.get('user_input', '')
+
+    if user_input.lower() == "exit":
+        response = "Exiting"
+    else:
+        response = handle_user_input(user_input)
+    
+    return jsonify({"response": response})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
